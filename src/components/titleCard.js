600f7b44d3ff523224dbfa95717fec95d8761a7b
Kevin VanDenBreemen <kevin.vandenbreemen@gmail.com>
2020-06-17 20:02:30 -0400

    Pull in material-ui for building my UIs

--- src/components/titleCard.js
+++ src/components/titleCard.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+import Box from '@material-ui/core/Box';
 
 class TitleCard extends React.Component {
 
     render() {
-        return <h2>{this.props.title}</h2>
+        return (
+            <Box color='green' bgcolor='yellow' fontSize='24pt'>
+                {this.props.title}
+            </Box>
+        );
     }
 
 }
